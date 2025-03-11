96ea38a2c038d42455357f13715c4585221774d4
Ilja Winkler <slayilja@gmail.com>
2025-03-11 12:03:36 +0100

    Add files via upload

---
+++ part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class AbsoluteValue {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int number = Integer.valueOf(scanner.nextLine());
+        
+        if(number < 0) {
+        //turn negative number into corresponding positive number
+            number = (number * -1);
+            System.out.println(number);
+        } else {
+            System.out.println(number);
+        }
+    }
+}
